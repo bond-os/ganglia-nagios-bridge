f857902726c9ab417c292f867297a702719296d5
Bernard Bondos <bernard@bond-os.com>
2014-06-16 09:40:32 +0200

    add lock file to prevent simultaneous execution

--- ganglia-nagios-bridge.sh
+++ ganglia-nagios-bridge.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 ENABLED=1
 
+LOCKFILE=/tmp/ganglia-nagios-bridge.lock
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if [[ $ENABLED -eq 1 ]] ; then
-	$DIR/ganglia-nagios-bridge.py
+	if [[ ! -f $LOCKFILE ]] ; then
+		touch $LOCKFILE
+		$DIR/ganglia-nagios-bridge.py
+		rm -rf $LOCKFILE
+	fi
 fi
